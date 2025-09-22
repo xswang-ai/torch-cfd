50c5fc57e08fc46430381792b4e3a22f4068802a
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-09-22 13:54:44 +1000

    generating data with bigger Re

--- fno/data_gen/test_navierstokes_jobs.sh
+++ fno/data_gen/test_navierstokes_jobs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script produces 5.2k training, 1.3k valid, and 1.3k test trajectories of the Navier-Stokes dataset.
 
-#SBATCH --time=00:30:00
+#SBATCH --time=06:30:00
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
@@ -23,15 +23,15 @@ source $HOME/.venvs/pytorch/bin/activate
 # training
 # python3 data_gen_McWilliams2d.py --num-samples 5000 --batch-size 256 --grid-size 256 --subsample 2 --visc 1e-3 --dt 1e-3 --time 10 --time-warmup 4.5 --num-steps 100 --diam "2*torch.pi"
 # validation
-python3 data_gen_McWilliams2d.py --num-samples 256 --batch-size 256 --grid-size 256 --subsample 2 --visc 1e-3 --dt 1e-3 --time 10 --time-warmup 4.5 --num-steps 100 --diam "2*torch.pi" --seed 0
+# python3 data_gen_McWilliams2d.py --num-samples 256 --batch-size 256 --grid-size 256 --subsample 2 --visc 1e-3 --dt 1e-3 --time 10 --time-warmup 4.5 --num-steps 100 --diam "2*torch.pi" --seed 0
 # test
-python3 data_gen_McWilliams2d.py --num-samples 500 --batch-size 256 --grid-size 256 --subsample 2 --visc 1e-3 --dt 1e-3 --time 10 --time-warmup 4.5 --num-steps 100 --diam "2*torch.pi" --seed 500
+# python3 data_gen_McWilliams2d.py --num-samples 500 --batch-size 256 --grid-size 256 --subsample 2 --visc 1e-3 --dt 1e-3 --time 10 --time-warmup 4.5 --num-steps 100 --diam "2*torch.pi" --seed 500
 
 
 # McWilliams 2d Re=5000
 # training
-# python data_gen_McWilliams2d.py --num-samples 1152 --batch-size 128 --grid-size 512 --subsample 4 --Re 5e3 --dt 5e-4 --time 10 --time-warmup 4.5 --num-steps 100 --diam "2*torch.pi"
+python data_gen_McWilliams2d.py --num-samples 5000 --batch-size 256 --grid-size 512 --subsample 4 --Re 5e3 --dt 5e-4 --time 10 --time-warmup 4.5 --num-steps 100 --diam "2*torch.pi"
 # validation
-# python data_gen_McWilliams2d.py --num-samples 256 --batch-size 256 --grid-size 512 --subsample 4 --Re 5e3 --dt 5e-4 --time 10 --time-warmup 4.5 --num-steps 100 --diam "2*torch.pi" --seed 0
+python data_gen_McWilliams2d.py --num-samples 256 --batch-size 256 --grid-size 512 --subsample 4 --Re 5e3 --dt 5e-4 --time 10 --time-warmup 4.5 --num-steps 100 --diam "2*torch.pi" --seed 0
 # # test
-# python data_gen_McWilliams2d.py --num-samples 500 --batch-size 256 --grid-size 512 --subsample 4 --Re 5e3 --dt 5e-4 --time 10 --time-warmup 4.5 --num-steps 100 --diam "2*torch.pi" --seed 500
+python data_gen_McWilliams2d.py --num-samples 500 --batch-size 256 --grid-size 512 --subsample 4 --Re 5e3 --dt 5e-4 --time 10 --time-warmup 4.5 --num-steps 100 --diam "2*torch.pi" --seed 500
