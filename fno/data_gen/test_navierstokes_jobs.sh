81385dfbf540cd82aa4f324962db78388ae72a02
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-10-08 19:32:24 +1100

    Increased runtime in Navier-Stokes job script from 1 hour to 2 hours and commented out validation sample generation command for McWilliams 2d configuration.

--- fno/data_gen/test_navierstokes_jobs.sh
+++ fno/data_gen/test_navierstokes_jobs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script produces 5.2k training, 1.3k valid, and 1.3k test trajectories of the Navier-Stokes dataset.
 
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
@@ -35,6 +35,6 @@ source $HOME/.venvs/pytorch/bin/activate
 # training
 # python data_gen_McWilliams2d.py --num-samples 1000 --batch-size 256 --grid-size 512 --subsample 4 --Re 5e3 --dt 5e-4 --time 10 --time-warmup 4.5 --num-steps 30 --diam "2*torch.pi"
 # # validation
-python data_gen_McWilliams2d.py --num-samples 256 --batch-size 256 --grid-size 512 --subsample 4 --Re 5e3 --dt 5e-4 --time 10 --time-warmup 4.5 --num-steps 30 --diam "2*torch.pi" --seed 0
+# python data_gen_McWilliams2d.py --num-samples 256 --batch-size 256 --grid-size 512 --subsample 4 --Re 5e3 --dt 5e-4 --time 10 --time-warmup 4.5 --num-steps 30 --diam "2*torch.pi" --seed 0
 # # # test
 python data_gen_McWilliams2d.py --num-samples 512 --batch-size 256 --grid-size 512 --subsample 4 --Re 5e3 --dt 5e-4 --time 10 --time-warmup 4.5 --num-steps 30 --diam "2*torch.pi" --seed 500
